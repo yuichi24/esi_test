b5447546c49ddb3cd43c7dbf680cd0c30933c7e7
yuichi24 <yuichi24@gmail.com>
2017-01-20 04:16:27 +0000

    debug

--- fwstat.sh
+++ fwstat.sh
@@ -2,23 +2,41 @@
 
 source openrc_fw
 
+CODE=$( curl -s "${EP_ESI}/firewalls/$1" \
+            -X GET -H "X-Auth-Token:${TEST_TOKEN}" -H "Accept:application/json" \
+            -H "Content-Type:application/json" \
+	| grep HTTP )
+echo ${CODE}
+
 while true ;do
-	sleep 10
-	local STATUS=$( curl -s ${EP_NETWORK}/v2.0/firewalls/$1" \
-		-X GET -H "X-Auth-Token:${TOKEN}" -H "Accept:application/json" \
-		-H "Content-Type:application/json" \
-		| jq -r ".firewall.status" )
-	echo ${STATUS}
-	case "${STATUS}" in
+	STATUS=$( curl -s "${EP_ESI}/firewalls/$1" \
+            -X GET -H "X-Auth-Token:${TEST_TOKEN}" -H "Accept:application/json" \
+            -H "Content-Type:application/json" \
+            | jq -r ".firewall.status" )
+        echo -n ${STATUS};
+        case "${STATUS}" in
+		PENDING*)
+		;;
 		ERROR)
 			break
-			;;
+		;;
 		ACTIVE)
 			break
-			;;
-	esac
+		;;
+		*)
+			break
+		;;
+        esac
+	echo -n "  ";
+	echo -n `date +"%Y/%m/%d %H:%M:%S"`;
+	echo ;
+	echo -n wait;sleep 1;
+	for i in `seq 10` ;do
+		echo -n .;sleep 1;
+	done
 done
 
-	echo "### Finish task ###"
-}
+echo ;echo
+echo "### fwstat.sh Finish ###"
+
 
